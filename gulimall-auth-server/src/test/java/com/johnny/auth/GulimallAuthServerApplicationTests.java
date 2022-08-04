adcb88f194760485874f8835b297b87b9271e5f3
JionLin <250912986@qq.com>
2022-08-04 22:04:15 +0800

    初始化搭建项目,只是一个框架。

---
+++ gulimall-auth-server/src/test/java/com/johnny/auth/GulimallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
